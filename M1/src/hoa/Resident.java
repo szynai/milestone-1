0b98606a4d9bbd5af7ad48ea11e0120efc3a9873
Mary Faith Salvador <salvadormaryfaith25@gmail.com>
2026-04-09 17:23:51 +0800

    resident-class-feature

--- M1/src/hoa/Resident.java
+++ M1/src/hoa/Resident.java
@@ -1,6 +1,17 @@
 
 package hoa;
 
-public class Resident {
-    
+public class Resident extends Person {
+
+    private String address;
+    private String email;
+
+    public Resident(String name, String contact, String address, String email) {
+        super(name, contact);
+        this.address = address;
+        this.email = email;
+    }
+
+    public String getAddress() { return address; }
+    public String getEmail() { return email; }
 }
